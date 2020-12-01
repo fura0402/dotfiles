ee71c9c3adfdf763faae7b85323d99000b83a073
fura0402 <58800801+fura0402@users.noreply.github.com>
2020-12-01 11:42:17 +0900

    Add:install_brew.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,10 @@ cd $BASEDIR
 echo 'Paste the Symbolic link...'
 for f in .??*; do
 	[ "$f" = ".DS_Store" ] && continue
-    [ "$f" = ".git" ] && continue
-    [ "$f" = ".gitignore" ] && continue
-    # シンボリックリンクを貼る
-    ln -snfv ${PWD}/"$f" ~/
+	[ "$f" = ".git" ] && continue
+	[ "$f" = ".gitignore" ] && continue
+
+	ln -snfv ${PWD}/"$f" ~/
 done
 
 ln -snfv ~/dotfiles/nvim/ ~/.config/
