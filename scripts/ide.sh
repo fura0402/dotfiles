d7e064a39341a763d29c8d2011c6a7d3978c095b
fura0402 <58800801+fura0402@users.noreply.github.com>
2021-01-06 00:12:16 +0900

    Update: adjustment;

--- scripts/ide.sh
+++ scripts/ide.sh
@@ -3,12 +3,12 @@
 # Creating an ide layout
 
 if [ ! $TMUX ]; then
-    echo "Cannot run without being in tmux"
+    echo "Cannot run without being in tmux."
 else
     if [ "$#" -eq 0 ]; then
-        echo -e "\n  Please enter an argument"
-        echo    "  list : ros"
-        echo    "         low"
+        echo -e "\n  Please enter an argument."
+        echo    "  list : ros;"
+        echo    "         low;"
         echo -en "\n"
     else
         case $1 in
@@ -25,18 +25,19 @@ else
             "low")
                 tmux rename-window low
                 tmux split-window -h
-                tmux resize-pane -L 43
+                tmux resize-pane -L 53
                 tmux select-pane -L
                 tmux split-window -v
                 tmux swap-pane -U
                 tmux clock
                 tmux resize-pane -U 19
                 tmux set-option pane-border-format "#[fg=colour0,bg=colour7](#{b:pane_current_path}) #P #(echo $SHELL)"
-                export PS1="\033[1;33m\]\W \033[01;32m\]\$ \e[m\]"
-                tmux select-pane -t 3
+                export PS1="\033[01;32m\]\$ \e[m\]"
+                tmux select-pane -t 1
+                tmux select-pane -t 2
                 clear
                 ;;
-            *) echo "Invalid argument : $1" ;;
+            *) echo "Invalid argument : $1." ;;
         esac
     fi
 fi
