bb4d71ddfa73883f332569bd2f31c97abd0a5cf3
fura0402 <58800801+fura0402@users.noreply.github.com>
2021-09-23 19:59:24 +0900

    Add: system_preferences

---
+++ installer/src/darwin/modules/system_preferences/system_conf/accessibility.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+exit 0
+###############################################################################
+# Accessibility                                                               #
+###############################################################################
+
+
+defaults write com.apple.Accessibility EnhancedBackgroundContrastEnabled -bool false
