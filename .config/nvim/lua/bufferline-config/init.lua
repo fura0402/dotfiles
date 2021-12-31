a485fdbc6e6e5de4f9de609f2f1694ae9f84e558
fura0402 <58800801+fura0402@users.noreply.github.com>
2021-12-31 21:28:37 +0900

    Update

--- .config/nvim/lua/bufferline-config/init.lua
+++ .config/nvim/lua/bufferline-config/init.lua
@@ -1,6 +1,6 @@
 require("bufferline").setup{}
 vim.cmd([[
-nnoremap <silent>bd :bdelete<CR>
+nnoremap <silent><Leader>bd :bdelete<CR>
 nnoremap <silent><Tab> :BufferLineCycleNext<CR>
 nnoremap <silent><S-Tab> :BufferLineCyclePrev<CR>
 ]])
