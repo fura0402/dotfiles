102ea91e8a10c0befa4b3872534e3b211a6f0530
fura0402 <58800801+fura0402@users.noreply.github.com>
2022-12-13 00:08:27 +0900

    Update

--- .config/nvim/lua/core/config.lua
+++ .config/nvim/lua/core/config.lua
@@ -1,10 +1,9 @@
 vim.cmd('autocmd!')
 
 vim.cmd ([[
-    let g:loaded_python_provider = 0
-    let g:python3_host_prog = expand('~/nvim-python3/bin/python3')
-    let g:ruby_host_prog = expand('~/.anyenv/envs/rbenv/versions/3.0.2/bin/neovim-ruby-host')
-    let g:node_host_prog = expand('~/.anyenv/envs/nodenv/versions/16.13.1/bin/neovim-node-host')
+    let g:python3_host_prog = expand('~/.nvim-python3/bin/python3')
+    "let g:ruby_host_prog = expand('')   
+    "let g:node_host_prog = expand('')
 
     colorscheme ownDefault
 
