9cc97011c0af2089952d25277f8a601ecd85ea67
fura0402 <58800801+fura0402@users.noreply.github.com>
2023-04-20 00:19:17 +0900

    Update

--- .config/nvim/lua/modules/nvim-tree.lua
+++ .config/nvim/lua/modules/nvim-tree.lua
@@ -1,4 +1,14 @@
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+  api.config.mappings.default_on_attach(bufnr)
+  vim.keymap.set('n', '<C-f>', api.tree.close, opts('Close'))
+end
+
 require('nvim-tree').setup({
+  on_attach = on_attach,
   auto_reload_on_write = true,
   disable_netrw = true,
   hijack_netrw = false,
@@ -11,11 +21,6 @@ require('nvim-tree').setup({
   },
   view = {
     width = 30,
-    mappings = {
-      list = {
-        { key = '<C-f>', action = 'close' },
-      },
-    },
   },
   renderer = {
     add_trailing = true,
